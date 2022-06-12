883f5c5f301a12f7a98d24820d4aeb6268370c4d
AdmiralGallade <mailforkushagra2@gmail.com>
2022-06-11 23:03:00 -0400

    FIXXED

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 
+
 import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
-#stopped at 0N__
+
+
 driver = webdriver.Chrome()
 
 driver.get('https://eclass.yorku.ca/eclass/')
+
+f=open("account.txt","r")
+lines=f.readlines()
+username=lines[0]
+password=lines[1]
+f.close()
+
+# print("Username: "+username +"\n Password: "+password+"\n")
+
+
+User_Input=driver.find_element_by_id("mli")
+User_Input.send_keys(username)
+
+# driver.find_element_by_id("password").click()
+
+
+
+# driver.find_element_by_id("password").send_keys("Test")
+
+driver.switch_to.alert.dismiss()
+
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "password"))).click()
+Pwd_Input=driver.find_element_by_id(password)
+Pwd_Input.send_keys("passwor1d")
 
 
 
